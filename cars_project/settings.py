7ee65e060e99ac5b46cd2618134cae4e075ff506
Luis Forbes <lu@Luiss-iMac.local>
2023-02-08 17:04:33 -0500

    changed database settings + created cars app

--- cars_project/settings.py
+++ cars_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c5&jq14#0+c--i8wiezk^6gk@27b4*k@a8^rn(3a1r%4*fh0m1'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +69,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cars_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -121,3 +113,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from cars_project.local_settings import *
+except ImportError:
+    pass
